5c94ef83248eb932739c4f354daf690cadbf020a
Taiki Nakatsuka <gndpig@gndpig.local>
2012-10-22 13:58:01 +0900

    add devise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,14 @@
 #coding: utf-8
 class MessagesController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @messages = Message.order('created_at desc')
   end
 
   def create
-    @message = Message.create!(params[:message])
+
+    @message = current_user.messages.new
+    @message.content = params[:message][:content]
+    @message.save
   end
 end
