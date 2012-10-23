dee856e57c80fe331a3401119bddecfddf3631b5
Taiki Nakatsuka <gndpig@gndpig.local>
2012-10-23 11:49:34 +0900

    チャット機能の強化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@
 class MessagesController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @messages = Message.order('created_at desc')
+    @messages = Message.order('created_at desc').limit(5) 
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022031402) do
+ActiveRecord::Schema.define(:version => 20121022050746) do
 
   create_table "messages", :force => true do |t|
     t.string   "content",    :null => false
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(:version => 20121022031402) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "last_name",                              :null => false
+    t.string   "first_name",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
